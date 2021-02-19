61e6443fe2382dc5ff6b8f2191b31b8fa5d97370
holysyid <holysyid@gmail.com>
2021-02-19 22:09:46 +0700

    yay

---
+++ src/main/kotlin/first-step.kt
@@ -0,0 +1,22 @@
+fun main() {
+    println("bismillah kotlin")
+    //this is my exercise with kotlin
+    /*fundamental kotlin
+    i assume myself can do basic code
+     */
+    declaration()
+}
+fun declaration(){
+    //this is how you declare simple things in kotlin
+    var a : String = "kotlin"
+    println(a)
+    var c : Int?
+    c = 10
+    println(c)
+
+    //to avoid error because of null, use ? after the data type
+    var b : String?
+    b = null
+    println(b)
+
+}
